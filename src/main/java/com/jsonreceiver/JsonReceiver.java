92cc0b236560bbdf0e23d3dda117361565c0dbb4
Kolege <a.kolege@gmail.com>
2017-06-19 17:31:18 +0300

    added 2 functions

--- src/main/java/com/jsonreceiver/JsonReceiver.java
+++ src/main/java/com/jsonreceiver/JsonReceiver.java
@@ -30,6 +30,69 @@ public class JsonReceiver {
 
     }
 
+    public static JSONObject getJSONFromUrl(String url1) {
+
+        try {
+            URL url = new URL(url1);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+
+            conn.setDoInput(true);
+            conn.connect();
+            is = conn.getInputStream();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(is, "utf-8"), 8);
+            StringBuilder sb = new StringBuilder();
+            String line = null;
+            while ((line = reader.readLine()) != null) {
+                sb.append(line + "\n");
+            }
+            is.close();
+            json = sb.toString();
+        } catch (Exception e) {
+            Log.e("Buffer Error", "Error converting result " + e.toString());
+        }
+
+        try {
+            jObj = new JSONObject(json);
+        } catch (JSONException e) {
+            Log.e("JSON Parser", "Error parsing data " + e.toString());
+        }
+        return jObj;
+    }
+
+    public static String getStringFromUrl(String url1) {
+
+        try {
+            URL url = new URL(url1);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setDoInput(true);
+            conn.connect();
+            is = conn.getInputStream();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(is, "utf-8"), 8);
+            StringBuilder sb = new StringBuilder();
+            String line = null;
+            while ((line = reader.readLine()) != null) {
+                sb.append(line + "\n");
+            }
+            is.close();
+            json = sb.toString();
+        } catch (Exception e) {
+            Log.e("Buffer Error", "Error converting result " + e.toString());
+        }
+
+        return json;
+    }
 
     /**
      * Issue a POST request to the server.
@@ -38,15 +101,13 @@ public class JsonReceiver {
      * @param params   request parameters.
      * @throws IOException propagated from POST.
      */
-    public static String post(String endpoint, Map<String, String> params){
-
-        Log.v("TAG", "endpoint: '" + endpoint + "' params: " + params.toString());
-        String message="";
+    public static String postAndGetStringResponse(String endpoint, Map<String, String> params) {
+        String message = "";
         URL url;
         try {
             url = new URL(endpoint);
         } catch (MalformedURLException e) {
-            throw new IllegalArgumentException("invalid filePlace: " + endpoint);
+            throw new IllegalArgumentException("invalid url: " + endpoint);
         }
         StringBuilder bodyBuilder = new StringBuilder();
         Iterator<Map.Entry<String, String>> iterator = params.entrySet().iterator();
@@ -59,25 +120,25 @@ public class JsonReceiver {
             }
         }
         String body = bodyBuilder.toString();
-        //Log.v(TAG, "Posting '" + body + "' to " + filePlace);
+        //Log.v(TAG, "Posting '" + body + "' to " + url);
         byte[] bytes = body.getBytes();
         HttpURLConnection conn = null;
         try {
-            //Log.e("URL", "> " + filePlace);
+            //Log.e("URL", "> " + url);
             conn = (HttpURLConnection) url.openConnection();
             conn.setDoOutput(true);
             conn.setUseCaches(false);
             conn.setFixedLengthStreamingMode(bytes.length);
             conn.setRequestMethod("POST");
             conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded;charset=UTF-8");
-            // post the request
+            // postAndGetStringResponse the request
             OutputStream out = conn.getOutputStream();
             out.write(bytes);
             out.close();
             // handle the response
 
             int status = conn.getResponseCode();
-            message=conn.getResponseMessage();
+            message = conn.getResponseMessage();
             if (status != 200) {
                 throw new IOException("Post failed with error code " + status);
             }
@@ -93,10 +154,9 @@ public class JsonReceiver {
 
             //Log.e("resp", "> " + response.toString());
             return response.toString();
-        }catch (IOException e){
+        } catch (IOException e) {
             e.printStackTrace();
-        }
-        finally {
+        } finally {
             if (conn != null) {
                 conn.disconnect();
             }
@@ -104,50 +164,78 @@ public class JsonReceiver {
         return message;
     }
 
-
-    public static JSONObject getJSONFromUrl(String url1) {
-
-
-        //Log.e("JSONParser:32", "Making request to: " + filePlace);
-        // Making HTTP request
+    /**
+     * Issue a POST request to the server.
+     *
+     * @param endpoint POST address.
+     * @param params   request parameters.
+     * @throws IOException propagated from POST.
+     */
+    public static JSONObject postAndGetJsonResponse(String endpoint, Map<String, String> params) {
+        String message = "";
+        URL url;
         try {
-            URL url = new URL(url1);
-            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-            //conn.setReadTimeout(10000 /* milliseconds */);
-            //conn.setConnectTimeout(15000 /* milliseconds */);
-            //conn.setRequestMethod("GET");
-            conn.setDoInput(true);
-            conn.connect();
-            is = conn.getInputStream();
+            url = new URL(endpoint);
+        } catch (MalformedURLException e) {
+            throw new IllegalArgumentException("invalid url: " + endpoint);
+        }
+        StringBuilder bodyBuilder = new StringBuilder();
+        Iterator<Map.Entry<String, String>> iterator = params.entrySet().iterator();
+        // constructs the POST body using the parameters
+        while (iterator.hasNext()) {
+            Map.Entry<String, String> param = iterator.next();
+            bodyBuilder.append(param.getKey()).append('=').append(param.getValue());
+            if (iterator.hasNext()) {
+                bodyBuilder.append('&');
+            }
+        }
+        String body = bodyBuilder.toString();
+        //Log.v(TAG, "Posting '" + body + "' to " + url);
+        byte[] bytes = body.getBytes();
+        HttpURLConnection conn = null;
+        try {
+            //Log.e("URL", "> " + url);
+            conn = (HttpURLConnection) url.openConnection();
+            conn.setDoOutput(true);
+            conn.setUseCaches(false);
+            conn.setFixedLengthStreamingMode(bytes.length);
+            conn.setRequestMethod("POST");
+            conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded;charset=UTF-8");
+            // postAndGetStringResponse the request
+            OutputStream out = conn.getOutputStream();
+            out.write(bytes);
+            out.close();
+            // handle the response
 
+            int status = conn.getResponseCode();
+            message = conn.getResponseMessage();
+            if (status != 200) {
+                throw new IOException("Post failed with error code " + status);
+            }
+
+            BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+            String inputLine;
+            StringBuffer response = new StringBuffer();
+
+            while ((inputLine = in.readLine()) != null) {
+                response.append(inputLine);
+            }
+            in.close();
+
+            //Log.e("resp", "> " + response.toString());
+            message=response.toString();
         } catch (IOException e) {
             e.printStackTrace();
-        }
-
-
-        try {
-            BufferedReader reader = new BufferedReader(new InputStreamReader(is, "utf-8"), 8);
-            StringBuilder sb = new StringBuilder();
-            String line = null;
-            while ((line = reader.readLine()) != null) {
-                sb.append(line + "\n");
+        } finally {
+            if (conn != null) {
+                conn.disconnect();
             }
-            is.close();
-            json = sb.toString();
-        } catch (Exception e) {
-            Log.e("Buffer Error", "Error converting result " + e.toString());
         }
-
-        // try parse the string to a JSON object
         try {
-            //Log.e("alp", json.toString());
-            jObj = new JSONObject(json);
+            jObj = new JSONObject(message);
         } catch (JSONException e) {
             Log.e("JSON Parser", "Error parsing data " + e.toString());
         }
-
-        // return JSON String
-
         return jObj;
     }
 }
